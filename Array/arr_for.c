27d8fa5960532fed188418c62676f6881579eb0a
Disha2025-Tech <dishak2029@gmail.com>
2025-09-23 19:23:57 +0530

    Add files via upload

---
+++ Array/arr_for.c
@@ -0,0 +1,19 @@
+// Printing Array using for loop
+
+#include <stdio.h>
+int main()
+{
+    int arr[]={1,2,3,45,6};
+
+    for(int i=0; i<=4; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
+//OUTPUT
+/*  1 2 3 45 6  */
